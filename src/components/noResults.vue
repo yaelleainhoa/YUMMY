fcfbbb25a997181243c4c3a8532daa51cf3b5aed
Yaelle <yaellesaraleguitran@gmail.com>
2022-04-23 16:55:40 +0200

    [Three] online model OK

--- src/components/noResults.vue
+++ src/components/noResults.vue
@@ -7,7 +7,7 @@
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export default {
   name: "Scene",
@@ -15,7 +15,8 @@ export default {
     return {
       scene: undefined,
       camera: undefined,
-      renderer: undefined
+      renderer: undefined,
+      container: undefined
     };
   },
   props: {
@@ -23,88 +24,76 @@ export default {
       type: String,
       required: true
     }
-    // modelSettings: {
-    //   type: Object,
-    //   required: true
-    // }
   },
-  created: function(){
-      this.scene = new THREE.Scene();
-            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-      const mesh = new THREE.Mesh( geometry, material );
-      this.scene.add( mesh );
+  mounted: 
+  async function(){
+    this.init();
+    this.renderScene();
   },
   methods: {
-    init() {
-        console.log("yoyo")
-      // this.scene = new THREE.Scene();
-      this.scene.background = null;
+    async init() {
+      console.log("INIT")
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color('salmon');
       this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
       this.renderer.setPixelRatio(window.devicePixelRatio);
       this.renderer.outputEncoding = THREE.sRGBEncoding;
-      const container = document.getElementById(this.containerId);
-      this.renderer.setSize(container.offsetWidth, container.offsetHeight);
-      container.appendChild(this.renderer.domElement);
-      // START: Adding camera
+      this.container = document.getElementById(this.containerId);
+      this.renderer.setSize(this.container.offsetWidth, this.container.offsetHeight);
+      this.container.appendChild(this.renderer.domElement);
       this.camera = new THREE.PerspectiveCamera(
         45,
-        container.offsetWidth / container.offsetHeight,
+        this.container.offsetWidth / this.container.offsetHeight,
         0.25,
         20
       );
-    // this.camera.position.set(this.modelSettings.cameraPosition[0], this.modelSettings.cameraPosition[1], this.modelSettings.cameraPosition[2]);
-      // END: Adding camera
-      // START: Adding controls
+      this.camera.position.set(0, 10,0);
       const controls = new OrbitControls(this.camera, this.renderer.domElement);
-      controls.minDistance = 2;
-      controls.maxDistance = 5;
-      // controls.enablePan = false;
+     controls.minDistance = 2;
+     controls.maxDistance = 10;
+      controls.enablePan = false;
       controls.target.set(0, 0, 0);
-      controls.addEventListener("change", this.renderScene); // use if there is no animation loop
-      // END: Adding controls
-      // START: Adding light
+      controls.addEventListener("change", this.renderScene); 
       const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
       directionalLight.position.set(0, 1, 0);
       directionalLight.castShadow = true;
       this.scene.add(directionalLight);
-      const light = new THREE.PointLight(0xffffcc, 1);
-      light.position.set(0, 600, 1000);
+      const lightpoint = new THREE.PointLight(0xff0000, 1);
+      lightpoint.position.set(0, 600, 1000);
+      this.scene.add(lightpoint);
+
+      var light = new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5);
+      light.position.set(0, 1, 0);
       this.scene.add(light);
-      const light2 = new THREE.PointLight(0xe6f7ff, 1);
-      light2.position.set(1000, 200, 0);
-      this.scene.add(light2);
-      const light3 = new THREE.PointLight(0xfff2e6, 1);
-      light3.position.set(0, 200, -1000);
-      this.scene.add(light3);
-      const light4 = new THREE.PointLight(0xc4c400, 1);
-      light4.position.set(-1000, 600, 1000);
-      this.scene.add(light4);
 
-      // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      // const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-      // const mesh = new THREE.Mesh( geometry, material );
-      // this.scene.add( mesh );
+      const geometry = new THREE.SphereGeometry( 0.5, 32, 16 );
+      const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+      const sphere = new THREE.Mesh( geometry, material );
+      sphere.position.set(0,0,1);
+      this.scene.add( sphere );
 
-      // END: Adding light
-      // START: Adding gtlf model
+      const loader = new GLTFLoader();
+      loader.crossOrigin = false;
+      let me = this;
+      let path = "../assets/fridge.glb"
 
-      // const loader = new GLTFLoader();
+      //https://s3-us-west-2.amazonaws.com/s.cdpn.io/39255/ladybug.gltf
 
-      // loader.load( './assets/gltf/scene.gltf', function ( gltf ) {
-      //   this.scene.add( gltf.scene );
-      // },
-      // undefined, function ( error ) {
-      //   console.log("ERROR", error);
-      // });
-
+      loader.load(path,
+      function(gltf){
+        if(gltf){
+          let object = gltf.scene;
+          object.position.set(0,0,0);
+          me.scene.add(object);
+        }
+      })
+      
     },
+
     renderScene() {
       this.renderer.render(this.scene, this.camera);
-    }
-  },
-  mounted() {
-    this.init();
+    },
+
   }
 };
 
@@ -112,7 +101,6 @@ export default {
 
 <style scoped>
 #noResults{
-    background-color: blue;
     width:500px;
     height: 500px;
 }
